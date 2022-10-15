c40a142377c2265a43bde2f8c7eb27faeeb1a655
rodrigoarias <rodrigo.arias@uala.com.ar>
2022-10-15 00:51:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const main = async (workspace) => {
+  const myToken = core.getInput('git-token');
+  const octokit = github.getOctokit(myToken);
+  const userTeamMap = core.getInput('user-team-map');
+  const owner = context.payload.repository.owner.login
+  const repo = context.payload.repository.name
+  const username = github.event.pull_request.user.login;
+  const prNumber = github.context.payload.number
+  
+  console.log(`owner ${owner} repo ${repo} username ${username} prNumber ${prNumber} `);
+
+  if(github.context.payload.pull_request && validActivityType(github.context.payload.action)) {
+    assignLabel(octokit, 'username', userTeamMap, owner, repo, prNumber);
+  } else {
+    core.setFailed('This action should only be runned on a opened Pull Request');
+  }
+}
+
+const validActivityType = (type) => {
+  validActivityTypes = ['opened'];
+  return validActivityTypes.includes(type);
+}
+
+
+const assignLabel = async (octokit, username, userTeamMap, owner, repo, prNumber) {
+  const team = userTeamMap[username];
+
+  if (team) {
+    console.log(`Adding label ${team} for ${username}`);
+    await octokit.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number: prNumber,
+      labels: [team]
+  })
+  } else {
+    console.log(`No label assigned for ${username}`);
+  }
+}
+
+
+
+main()
